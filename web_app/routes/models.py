cf2baa5700e11e136fc9921362eb427b8f3be417
Jason Robinson <robinsonjason761@gmail.com>
2021-02-08 21:18:26 -0600

    adding models file to database

---
+++ web_app/routes/models.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+migrate = Migrate()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+r
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.id} {self.title}>"
+    
+def parse_records(database_records):
+    """
+    A helper method for converting a list of database record objects into a list of dictionaries, so they can be returned as JSON
+
+    Param: database_records (a list of db.Model instances)
+
+    Example: parse_records(User.query.all())
+
+    Returns: a list of dictionaries, each corresponding to a record, like...
+        [
+            {"id": 1, "title": "Book 1"},
+            {"id": 2, "title": "Book 2"},
+            {"id": 3, "title": "Book 3"},
+        ]
+    """
+    parsed_records = []
+    for record in database_records:
+        print(record)
+        parsed_record = record.__dict__
+        del parsed_record["_sa_instance_state"]
+        parsed_records.append(parsed_record)
+    return parsed_records
